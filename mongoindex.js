8ef5f9ef181843314cb7d16aea08731c6ed4825b
Gosu Helper <gosuhelper@gmail.com>
2024-01-26 18:14:02 -0500

    mongo port

--- mongoindex.js
+++ mongoindex.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const PORT = 3000
 require('dotenv').config()
 
-module.exports = async () => {
+async function mongo(){
     mongoose.connect(process.env.MONGO)
     return mongoose
 }
 mongoose.connection.on('connected', () => {
     console.log("Successfully connected to mongo.")
 })
+
+mongo().then(() => {
+    app.listen(PORT, () => {
+        console.log("Listening for requests");
+    })
+})
+
+module.exports = mongo
