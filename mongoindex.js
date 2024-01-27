1667da1e5f6e0c74f2d7668d59701e3109eb6d2c
Gosu Helper <gosuhelper@gmail.com>
2024-01-26 19:16:41 -0500

    removed 300 port from mongoindex

--- mongoindex.js
+++ mongoindex.js
@@ -1,21 +1,10 @@
 const mongoose = require('mongoose')
-const express = require('express')
-const app = express()
-const PORT = 3000
 require('dotenv').config()
 
-async function mongo(){
+module.exports = async () => {
     mongoose.connect(process.env.MONGO)
     return mongoose
 }
 mongoose.connection.on('connected', () => {
     console.log("Successfully connected to mongo.")
 })
-
-mongo().then(() => {
-    app.listen(PORT, () => {
-        console.log("Listening for requests");
-    })
-})
-
-module.exports = mongo
