698a96d4d023facc981759b2c28881a987b7ca66
Gosu Helper <gosuhelper@gmail.com>
2024-02-27 10:25:05 -0500

    Leaderboard command in progress

---
+++ src/commands/commandList/Miscellaneous/leaderboard.js
@@ -0,0 +1,17 @@
+const CommandInterface = require('../../commandInterface')
+const {attachments} = require('discord.js')
+const {Font, RankCardBuilder} = require('canvacord')
+const levelSchema = require('../../../../Schema/levelSchema')
+
+module.exports = new CommandInterface({
+    alias: ['leaderboard', 'lb'],
+    args: '(user)',
+    desc: "Checks the leaderboard.",
+    related:["gh rank", "level"],
+    permissions:[],
+    permLevel: 'User',
+    group:["Miscellaneous"],
+    execute: async function(p){
+        p.send("In progress")
+    }
+})
